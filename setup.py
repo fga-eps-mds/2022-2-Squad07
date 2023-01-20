2b4f82bdc31ed2d8c9052c8be1497ffe101f770f
gitbmvb <bruno.martval@gmail.com>
2023-01-20 15:10:42 -0300

    fix: changing several settings in setup.py

--- setup.py
+++ setup.py
@@ -9,28 +9,28 @@ with open("README.md", "r") as fh:
 long_description = readme
   
 cmds = ["cmd/*.txt"]
-setup(
-        name ='ez_docs',
-        version ='1.0.0',
-        author ='Bruno Ribeiro, Bruno Martins, Diógenes Dantas, Igor Penha, Lucas Gobbi e Rafael Nobre',
-        author_email ='rafaelmedeirosnobre2001@gmail.com',
-        description ='Um pacote python de geração de documentos automáticos',
-        long_description = long_description,
-        long_description_content_type ="text/markdown",
-        license ='MIT',
-        packages = find_packages(),
-        package_data = {'ez_docs' : cmds },
-        entry_points ={
+setup (
+    name ='ez_docs',
+    version ='1.0.0',
+    author ='Bruno Ribeiro, Bruno Martins, Diógenes Dantas, Igor Penha, Lucas Gobbi e Rafael Nobre',
+    author_email ='rafaelmedeirosnobre2001@gmail.com',
+    url='https://github.com/fga-eps-mds/2022-2-ez-docs',
+    description ='Um pacote python de geração de documentos automáticos',
+    long_description = long_description,
+    long_description_content_type ="text/markdown",
+    license ='MIT',
+    keywords = ['ez_docs', 'ez-docs'],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    package_data = {'ez_docs' : cmds},
+    entry_points ={
             'console_scripts': [
                 'ez-docs= ez_docs.main:main'
             ]
-        },
-        classifiers =(
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-        ),
-        keywords =['ez_docs', 'ez-docs'],
-        install_requires = requirements,
-        zip_safe = False
+    },
+    classifiers = (
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
 )
