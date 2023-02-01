84537fdf2b4f5c973453835940f03e83ea79a3e0
gitbmvb <bruno.martval@gmail.com>
2023-01-31 21:52:07 -0300

    feat: adding constraints to data_cleaning

--- src/ez_docs/modules/data_cleaning.py
+++ src/ez_docs/modules/data_cleaning.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 #Finds out which non-alphanumeric character separates a given csv file
 def find_delimiter(location: str) -> str:
     delimiters_dict = {}
@@ -23,18 +24,24 @@ def filter_format(location: str) -> pd.DataFrame:
     elif extension == "xml": return pd.read_xml(location)
     else: raise Exception(f"\033[0;31mThe extension '{extension}' is not accepted.\nValid: csv, html, json, xlsx, xml.\033[0m")
 
+#Checks if a given line passes a specified condition
+def passes_constraint(const: list, dataset: pd.DataFrame, line):
+    if not const: return True
+    else:
+        if const[0] not in dataset.columns: raise Exception("")
+        if (type(line[const[0]]) is int and const[1] not in [">", ">=", "<", "<=", "==", "!="]) or (type(line[const[0]]) is str and const[1] not in ["==", "!="]): raise Exception("")
+        if (type(line[const[0]]) is int) and eval(f"{line[const[0]]} {const[1]} {const[2]}"): return True
+        if type(line[const[0]]) is str:
+            if("*" in const[2][0] and "*" in const[2][-1] and const[2].strip("*") in line[const[0]]): return True
+            elif(eval(f"'{line[const[0]]}' {const[1]} '{const[2]}'")): return True
+    return False
 
 # Converts the data_set to the format specified in the architecture
-def filter_data(location: str) -> list:
+def filter_data(location: str, constraint: list = None) -> list:
     try:
         start_data = filter_format(location)
         if True in start_data.isna().values: print("\033[0;33mWarning: Your dataset file has incomplete lines. These ones will be ignored.\033[0m")
         start_data.dropna(inplace=True)
         start_data.drop_duplicates(inplace=True)
-
-        final_data = []
-        for line in range(len(start_data)):
-            final_data.append({})
-            for col in list(start_data.columns): final_data[line][col] = str(start_data.iloc[line][col])
-        return final_data
+        return [{col: str(start_data.iloc[line][col]) for col in start_data.columns} for line in range(len(start_data)) if passes_constraint(constraint, start_data, start_data.iloc[line])]
     except FileNotFoundError: raise Exception("\033[0;31mThe dataset file was not found.\nPlease, make sure you have typed a correct file name.\033[0m")
